90b2e10bce1a6a36bfbe9e75834614b492ac9309
faiq_khan <shayankhan480@gmail.com>
2020-02-19 13:36:39 +0500

    add React redux & create a store

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
