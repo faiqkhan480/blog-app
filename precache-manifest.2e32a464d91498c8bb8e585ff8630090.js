9741556a68502810d676e06e3804bd02af85d61f
faiq_khan <shayankhan480@gmail.com>
2020-02-22 12:21:06 +0500

    Updates

---
+++ precache-manifest.2e32a464d91498c8bb8e585ff8630090.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da1da1c86029b9d3f5d72ded07bf1fb",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "2f26bd5797dbcb6b7c86",
+    "url": "/blog-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aa254fb58629c06e1b87",
+    "url": "/blog-app/static/css/main.eb54c6f4.chunk.css"
+  },
+  {
+    "revision": "2f26bd5797dbcb6b7c86",
+    "url": "/blog-app/static/js/2.58308513.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/blog-app/static/js/2.58308513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa254fb58629c06e1b87",
+    "url": "/blog-app/static/js/main.a3103581.chunk.js"
+  },
+  {
+    "revision": "1cd53264b2414341795c",
+    "url": "/blog-app/static/js/runtime-main.ccea3504.js"
+  }
+]);
